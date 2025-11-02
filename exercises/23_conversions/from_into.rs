7c630952e9c97e09f2f33699fb683be20922c57a
Calvin Johnson <calvinofthejs@gmail.com>
2025-11-02 14:57:53 +1000

    23_conversions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -34,7 +34,28 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let items: Vec<&str> = s.split(',').collect();
+
+        if items.len() != 2 {
+            Self::default()
+        } else {
+            let name_str = items[0];
+            let age_str = items[1];
+
+            if name_str.is_empty() {
+                Self::default()
+            } else {
+                match age_str.parse::<u8>() {
+                    Ok(age) => Person {
+                        name: String::from(name_str),
+                        age,
+                    },
+                    Err(_) => Self::default(),
+                }
+            }
+        }
+    }
 }
 
 fn main() {
